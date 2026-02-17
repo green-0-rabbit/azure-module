d6adb4b4988f8c0b570e82f915cc14a043e9b244
green-0-rabbit <80008839+green-0-rabbit@users.noreply.github.com>
2026-02-17 21:34:55 +0100

    fix: Update resource group name references to use variable instead of data source (#12)

--- keyvault/main.tf
+++ keyvault/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_key_vault" "this" {
   name                          = var.key_vault_name
   location                      = var.location
-  resource_group_name           = data.azurerm_resource_group.target.name
+  resource_group_name           = var.resource_group_name
   tenant_id                     = data.azurerm_client_config.current.tenant_id
   sku_name                      = lower(var.sku_name)
   public_network_access_enabled = var.public_network_access_enabled
